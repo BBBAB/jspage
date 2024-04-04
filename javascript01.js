d7ec51ce3904e4b70112271c07350c5bdce63d2c
BBBAB <kevinwang@naver.com>
2024-04-04 17:43:33 +0900

    feat: create javascript01.js

---
+++ javascript01.js
@@ -0,0 +1,26 @@
+// 자바스크립트 변수, 자료형, 함수, 조건문, 반복문
+
+// 변수
+let creamBread ='a';
+const chocoBread = 'b';
+
+
+let camelcase;
+let snake_case;
+let PascalCase
+let %case1;
+let _case2;
+
+let bread = '빵'
+let creamBread = '크림빵'
+bread = creamBread
+console.log(bread)
+console.log('첫 번째 빵은 : ${bread}')
+console.log('두 번째 빵은 : ${bread}')
+// 자료형
+
+// 함수
+
+// 조건문
+
+// 반복문
